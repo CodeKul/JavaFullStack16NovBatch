a0fd1fac39c1b36ccda4ebdb7e8a03963ccd6569
Madhuri <codekul.madhuri@gmail.com>
2022-02-01 14:40:42 +0530

    Madhuri - multithreading, create thread

--- src/multithreading/ThreadForLoop.java
+++ src/multithreading/ThreadForLoop.java
@@ -4,15 +4,29 @@ public class ThreadForLoop extends Thread{
     @Override
     public void run() {
         for (int i=1;i<=10;i++){
-            System.out.println(Thread.currentThread().getName()+" i="+i);
+            try {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName()+" i="+i);
+            }catch (InterruptedException e){
+                System.out.println(e);
+            }
         }
         for (int j=1;j<=10;j++){
-            System.out.println(Thread.currentThread().getName()+"j "+j);
+            try {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName()+"j "+j);
+            }catch (InterruptedException e){
+                System.out.println(e);
+            }
         }
     }
 
     public static void main(String[] args) {
         ThreadForLoop th1 = new ThreadForLoop();
         th1.start();
+        th1.setName("TH1");
+        ThreadForLoop th2 = new ThreadForLoop();
+        th2.start();
+        th2.setName("TH2");
     }
 }
--- src/multithreading/ThreadNaming.java
+++ src/multithreading/ThreadNaming.java
@@ -2,17 +2,17 @@ package multithreading;
 
 public class ThreadNaming extends Thread {
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        System.out.println("in run method "+Thread.currentThread().getName());
        // System.out.println("running");
 
     }
 
     public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName());
+        System.out.println("main method "+Thread.currentThread().getName());
      //   Thread.currentThread().setName("App1");
         ThreadNaming nm = new ThreadNaming();
         nm.start();
         nm.setName("Codekul");
-        System.out.println(Thread.currentThread().getName());
+        System.out.println("main method "+Thread.currentThread().getName());
     }
 }
